93aed2c4708d9a39b9b2cbd60a445282b1984325
Volker Mische <volker.mische@gmail.com>
2010-12-22 16:38:26 +0100

    Removed 09 upgrade code (as it was removed from trunk).

--- src/geocouch/couch_spatial_group.erl
+++ src/geocouch/couch_spatial_group.erl
@@ -296,8 +296,6 @@ prepare_group({RootDir, DbName, #spatial_group{sig=Sig}=Group}, ForceReset)->
                 % this can happen if we missed a purge
                 {ok, reset_file(Db, Fd, DbName, Group)};
             true ->
-                % 09 UPGRADE CODE
-                ok = couch_file:upgrade_old_header(Fd, <<$r, $c, $k, 0>>),
                 case (catch couch_file:read_header(Fd)) of
                 {ok, {Sig, HeaderInfo}} ->
                     % sigs match!
