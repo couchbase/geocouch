e9025d596a19eed999e24ef7ff0fa3b3d10a8f4f
Volker Mische <vmx@couchbase.com>
2014-09-11 12:48:22 -0700

    Fix bounding box calculation
    The bounding box calculation expected tuples instead of lists. This
    broke the boudning box calculation of linestrings and polygons.
    
    Change-Id: Id2cab6521226022eae77c10340f3bcc09647a453
    Reviewed-on: http://review.couchbase.org/41366
    Reviewed-by: Volker Mische <volker.mische@gmail.com>
    Tested-by: Volker Mische <volker.mische@gmail.com>

--- gc-couchbase/src/spatial_view.erl
+++ gc-couchbase/src/spatial_view.erl
@@ -862,12 +862,11 @@ extract_bbox(Type, Coords, InitBbox) ->
 bbox([], Range) ->
     Range;
 bbox([[X, Y]|Rest], nil) ->
-    %bbox(Rest, [{X, X}, {Y, Y}]);
     bbox(Rest, [[X, X], [Y, Y]]);
 bbox([Coords|Rest], Range) ->
     Range2 = lists:zipwith(
-        fun(Coord, {Min, Max}) ->
-            {erlang:min(Coord, Min), erlang:max(Coord, Max)}
+        fun(Coord, [Min, Max]) ->
+            [erlang:min(Coord, Min), erlang:max(Coord, Max)]
         end, Coords, Range),
     bbox(Rest, Range2).
 
